6f4d04bb98f2b67aea3c394c58e6e16934dbc067
AdrianDris <adrianc200399@gmail.com>
2020-10-01 11:07:25 +0200

    Insert + Find_min

--- fibonacci_heap.py
+++ fibonacci_heap.py
@@ -33,30 +33,28 @@ class Heap(object):
         """
         Fusionne deux arbres
         """
-        pass
+        pass        
 
 
 class FibonacciHeap(Heap):
     """
-    Une fibonnaci heap est un arbre permettant de stocker et trier des donnés efficacement
-
-    https://en.wikipedia.org/wiki/Fibonacci_heap
-
-    L'implémentation est décrite en anglais : https://en.wikipedia.org/wiki/Fibonacci_heap#Implementation_of_operations
-    et en français : https://fr.wikipedia.org/wiki/Tas_de_Fibonacci#Implémentation_des_opérations
+    Chaque case dans l’arbre est une valeur
+    Exemple avec 1 seule valeur par branche (la racine)
+    1 - Trouver le minimum dans la liste des racines et retourner la valeur de cette variable
+    2 - insertion : insérons 0 dans l’arbre (a la suite de la liste) -> append
+    3 - Si la valeur ajouté est un nouveau minimum -> mettre à jour le minimum
+    4 - fusion de 2 arbres – ajouter le nouvel arbre dans la liste du précédent arbre (merge le 2eme dans le 1er) puis récupérer le minimum de l’arbre 2 et le comparer avec le minimum de l’arbre 1. Si la valeur du 2 n’est pas plus petit que le 1er (quand la valeur est supérieur) on peut oublier la valeur minimum du second arbre
+    5 - Si le minimum du second arbre était plus petit que celui du 1er, on aurait dû mettre à jour la valeur minimum
     """
 
+    def __init__(self):
+        self.tree = []
+
     def insert(self, value: int) -> None:
-        """
-        Ajoute une valeur dans l'arbre
-        """
-        pass
+        self.tree.append(value)
 
     def find_min(self) -> int:
-        """
-        Retourne la valeur minimum dans l'arbre
-        """
-        pass
+        return min(self.tree)
 
     def delete_min(self) -> int:
         """
@@ -64,8 +62,17 @@ class FibonacciHeap(Heap):
         """
         pass
 
-    def merge(self, fibonnaci_heap: Heap) -> None:
-        """
-        Fusionne deux arbres
-        """
+    def merge(self,fibonnaci_heap: Heap) -> None:
+        
         pass
+
+heap = FibonacciHeap()
+heap.insert(2)
+heap.insert(5)
+heap.insert(8)
+heap.insert(4)
+print(heap.tree)
+print('Plus petite valeur : {}\n'.format(heap.find_min()))
+heap.insert(0)
+print(heap.tree)
+print('Plus petite valeur : {}\n'.format(heap.find_min()))
